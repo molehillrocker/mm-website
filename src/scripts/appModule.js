e8fd17d8cd052940a49d75d18a694c4a6a13f73b
Lars Bischoff <molehillrocker@users.noreply.github.com>
2016-02-21 19:02:51 +0100

    [Bugfix] Correct misspelled method name 'contant' to 'constant'

--- src/scripts/appModule.js
+++ src/scripts/appModule.js
@@ -23,7 +23,7 @@
     .constant('LOG', {
       ENABLE_DEBUG: true
     })
-    .contant('WEATHER_SETTINGS', {
+    .constant('WEATHER_SETTINGS', {
       OPEN_WEATHER_MAP: {
         BASE_URI: 'http://api.openweathermap.org/data/2.5',
         API_KEY: '<api-key>',
