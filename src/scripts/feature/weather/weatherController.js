33a6bdf9d1bca3db5a728afdcdb4577264203ce7
Lars Bischoff <molehillrocker@users.noreply.github.com>
2016-02-23 19:00:17 +0100

    Use the '_weatherIconService' in the 'WeatherController'

--- src/scripts/feature/weather/weatherController.js
+++ src/scripts/feature/weather/weatherController.js
@@ -2,10 +2,13 @@
   'use strict';
 
   angular.module('app.feature.weather').controller('WeatherController', [
-    '$log', '$scope', 'OpenWeatherMapFactory', '_weatherService',
-    function($log, $scope, OpenWeatherMapFactory, _weatherService) {
+    '$log', '$scope', 'OpenWeatherMapFactory', '_weatherService', '_weatherIconService',
+    function($log, $scope, OpenWeatherMapFactory, _weatherService, _weatherIconService) {
+
       var WeatherInfo = OpenWeatherMapFactory.getWeatherInfo(function() {
-        $scope.weatherInfo = _weatherService.fromOpenWeatherMapWeatherInfo(WeatherInfo);
+        var currentWeatherInfo = _weatherService.fromOpenWeatherMapWeatherInfo(WeatherInfo);
+        $scope.currentWeatherInfo = currentWeatherInfo;
+        $scope.currentWeatherIconClass = _weatherIconService.fromOpenWeatherMapIconId(currentWeatherInfo.weather.iconName);
       });
 
       var WeatherForecast = OpenWeatherMapFactory.getWeatherForecast(function() {
