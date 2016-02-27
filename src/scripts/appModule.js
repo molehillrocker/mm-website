3dc2829d2eca728a3732ab5d2c9d45adb3a448c9
Lars Bischoff <molehillrocker@users.noreply.github.com>
2016-02-27 16:30:12 +0100

    Apply custom dark theming with a black background and white foreground

--- src/scripts/appModule.js
+++ src/scripts/appModule.js
@@ -9,12 +9,34 @@
   ]);
 
   angular.module('app').config([
-    '$logProvider', 'LOG', '$locationProvider',
-    function($logProvider, log, $locationProvider) {
+    '$logProvider', 'LOG', '$locationProvider', '$mdThemingProvider',
+    function($logProvider, log, $locationProvider, $mdThemingProvider) {
       // Enable or disable debug logging
       $logProvider.debugEnabled(log.ENABLE_DEBUG);
 
       $locationProvider.html5Mode(false);
+      var mmBlack = $mdThemingProvider.extendPalette('grey', {
+        '50': '000000',
+        '100': '000000',
+        '200': '000000',
+        '300': '000000',
+        '400': '000000',
+        '500': '000000',
+        '600': '000000',
+        '700': '000000',
+        '800': '000000',
+        '900': '000000',
+        'A100': '000000',
+        'A200': '000000',
+        'A400': '000000',
+        'A700': '000000'
+      });
+
+      $mdThemingProvider.definePalette('mm-black', mmBlack);
+
+      $mdThemingProvider.theme('default')
+        .dark()
+        .backgroundPalette('mm-black');
     }
   ]);
 
