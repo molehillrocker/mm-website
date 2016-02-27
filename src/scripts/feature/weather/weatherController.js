efd724cae833e3c16ebc0f4299b124d885d776bc
Lars Bischoff <molehillrocker@users.noreply.github.com>
2016-02-27 13:17:17 +0100

    Rework the weather controller and the corresponding HTML template

--- src/scripts/feature/weather/weatherController.js
+++ src/scripts/feature/weather/weatherController.js
@@ -7,8 +7,16 @@
 
       var currentWeather = _owmWeatherFactory.getCurrentWeather(function() {
         var currentWeatherInfo = _owmWeatherService.fromCurrentWeather(currentWeather);
-        $scope.currentWeatherInfo = currentWeatherInfo;
-        $scope.currentWeatherIconClass = _owmWeatherIconService.fromIconId(currentWeatherInfo.weather.iconId);
+        $scope.weatherIcon = _owmWeatherIconService.fromIconId(currentWeatherInfo.weather.iconId);
+        $scope.windIcon = _owmWeatherIconService.fromWindDirection(currentWeatherInfo.wind.direction);
+        $scope.location = currentWeatherInfo.name;
+        $scope.windSpeed = currentWeatherInfo.wind.speed;
+        $scope.sunrise = currentWeatherInfo.sunrise;
+        $scope.sunset = currentWeatherInfo.sunset;
+        $scope.temperature = currentWeatherInfo.weather.temperature;
+        $scope.humidity = currentWeatherInfo.weather.humidity;
+        $scope.precipitation = currentWeatherInfo.rainVolumeInLastThreeHours ||
+          currentWeatherInfo.snowVolumeInLastThreeHours || 0;
       });
 
       var dailyForecast = _owmWeatherFactory.getDailyForecast(function() {
