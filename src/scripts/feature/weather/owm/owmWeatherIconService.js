01e36002f26d245142883cc1ae89e119d5068ccd
Lars Bischoff <molehillrocker@users.noreply.github.com>
2016-02-27 13:15:21 +0100

    Use wind direction icon from weather-icons based on the value directly
    - It is not necessary to add our own calculation since weather-icons pro-
      vides CSS classes for all wind directions between 0° and 360°.

--- src/scripts/feature/weather/owm/owmWeatherIconService.js
+++ src/scripts/feature/weather/owm/owmWeatherIconService.js
@@ -16,41 +16,9 @@
       };
 
       this.fromWindDirection = function(degrees) {
-        if (!angular.isNumber(degrees)) {
-          return 'wi wi-fw wi-na';
-        }
-
-        if (degrees > 360) {
-          degrees = degrees % 360;
-        }
-
-        if (degrees > 337.5 && degrees <= 22.5) {
-          return 'wi-from-n';
-        }
-        if (degrees > 22.5 && degrees <= 67.5) {
-          return 'wi-from-ne';
-        }
-        if (degrees > 67.5 && degrees <= 112.5) {
-          return 'wi-from-e';
-        }
-        if (degrees > 112.5 && degrees <= 157.5) {
-          return 'wi-from-se';
-        }
-        if (degrees > 157.5 && degrees <= 202.5) {
-          return 'wi-from-s';
-        }
-        if (degrees > 202.5 && degrees <= 247.5) {
-          return 'wi-from-sw';
-        }
-        if (degrees > 247.5 && degrees <= 292.5) {
-          return 'wi-from-w';
-        }
-        if (degrees > 292.5 && degrees <= 337.5) {
-          return 'wi-from-nw';
-        } else {
-          $log.warn('Unknown wind direction: ' + degrees);
-          return 'wi wi-fw wi-na';
-        }
+        return !angular.isNumber(degrees)
+          ? 'wi wi-fw wi-wind wi-na'
+          : 'wi wi-fw wi-wind from-' + Math.round(degrees) % 360 + '-deg';
       };
     }
   ]);
