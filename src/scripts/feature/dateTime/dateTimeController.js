528675ccbd0ea2cb182152c2efdd284c81c2b5ce
Lars Bischoff <molehillrocker@noreply.users.github.com>
2016-03-03 10:09:59 +0100

    Set current date and time before starting the interval service
    - The interval service does not start immediately, instead it waits the
      configured amount of time before it runs the code that is declared inside
      the callback function.

--- src/scripts/feature/dateTime/dateTimeController.js
+++ src/scripts/feature/dateTime/dateTimeController.js
@@ -4,6 +4,9 @@
   angular.module('app.feature.dateTime').controller('DateTimeController', [
     '$scope', '$interval',
     function($scope, $interval) {
+      // Set the current date and time on startup
+      $scope.dateTime = Date.now();
+      // And afterwards every second in an endless loop
       $interval(function() {
         $scope.dateTime = Date.now();
       }, 1000);
