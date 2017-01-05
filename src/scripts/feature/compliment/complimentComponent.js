2bfef414267dcdf66e140ecd839d5be2b669e37a
Lars Bischoff <molehillrocker@users.noreply.github.com>
2017-01-05 16:47:49 +0100

    Build the compliment module as component

--- src/scripts/feature/compliment/complimentComponent.js
+++ src/scripts/feature/compliment/complimentComponent.js
@@ -1,4 +1,31 @@
-angular.module('app').component('mmCompliment', {
-  templateUrl: '/views/compliment.html',
-  controller: 'ComplimentController'
-})
+(function() {
+  'use strict';
+
+  angular.module('app.feature.compliment').component('mmCompliment', {
+    templateUrl: '/views/compliment.html',
+    controllerAs: 'vm',
+    controller: [
+      '$interval', '_complimentProvider',
+      function($interval, _complimentProvider) {
+        var getRandomCompliment = function() {
+          var now = new Date();
+          return _complimentProvider.provide(now.getHours());
+        };
+
+        // Set a random compliment every 5 minutes in an endless loop
+        var complimentInterval = $interval(function() {
+          this.compliment = getRandomCompliment();
+        }, 300000);
+
+        this.$onInit = function() {
+          // Set a random compliment on startup
+          this.compliment = getRandomCompliment();
+        }
+
+        this.$onDestroy = function() {
+          $interval.cancel(complimentInterval);
+        };
+      }
+    ]
+  });
+})();
