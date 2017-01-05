00498d9739f97855ebbe33949cc0857a8404fad8
Lars Bischoff <molehillrocker@users.noreply.github.com>
2017-01-05 16:48:01 +0100

    Build the dateTime module as component

--- src/scripts/feature/dateTime/dateTimeComponent.js
+++ src/scripts/feature/dateTime/dateTimeComponent.js
@@ -1,4 +1,26 @@
-angular.module('app').component('mmDatetime', {
-  templateUrl: '/views/dateTime.html',
-  controller: 'DateTimeController'
-})
+(function() {
+  'use strict';
+
+  angular.module('app.feature.dateTime').component('mmDatetime', {
+    templateUrl: '/views/dateTime.html',
+    controllerAs: 'vm',
+    controller: [
+      '$interval',
+      function($interval) {
+        // Set the current date and time every second in an endless loop
+        var datetimeInterval = $interval(function() {
+          this.dateTime = Date.now();
+        }, 1000);
+
+        this.$onInit = function() {
+          // Set the current date and time on startup
+          this.dateTime = Date.now();
+        }
+
+        this.$onDestroy = function() {
+          $interval.cancel(datetimeInterval);
+        };
+      }
+    ]
+  })
+})();
